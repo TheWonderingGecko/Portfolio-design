053cafb0ea4306364a901515fa597548d5500a04
JahmirKS <98054292+JahmirKS@users.noreply.github.com>
2023-04-09 02:07:07 -0500

    project completed

--- src/components/Project_Card.jsx
+++ src/components/Project_Card.jsx
@@ -1,14 +1,27 @@
 const Project_Card = (props) => {
   return (
-    <div className="flex flex-col justify-center gap-5 my-10">
-      <img src={props.image} alt={props.title} />
+    <div className="relative flex flex-col justify-center gap-5 my-10 ">
+      <div className="relative group">
+        <img src={props.image} alt={props.title} />
+        <div className="absolute top-0 items-center justify-center hidden w-full h-full bg-black/90 group-hover:flex">
+          <div className="flex flex-col items-center justify-center gap-10 font-bold text-center uppercase text-m_sm leading-m_sm tracking-m_sm">
+            <span className="pb-4 border-b-2 border-underlined hover:text-underlined">
+              <a href="#">View project</a>
+            </span>
+            <span className="pb-4 border-b-2 border-underlined hover:text-underlined w-fit">
+              <a href="#">View code</a>
+            </span>
+          </div>
+        </div>
+      </div>
       <h3 className="font-bold uppercase text-m leading-m ">{props.title}</h3>
       <div className="flex gap-4 -mt-4 font-semibold uppercase text-blg leading-blg text-sub_text_color">
         {props.skills.map((skill) => {
           return <span>{skill}</span>
         })}
       </div>
-      <div className="flex gap-5 font-bold uppercase text-m_sm leading-m_sm tracking-m_sm ">
+
+      <div className="flex gap-5 font-bold uppercase text-m_sm leading-m_sm tracking-m_sm lg:hidden ">
         <span className="pb-4 border-b-2 border-underlined hover:text-underlined">
           <a href="#">View project</a>
         </span>
--- src/data.jsx
+++ src/data.jsx
@@ -4,36 +4,75 @@ import project_3 from './assets/images/thumbnail-project-3-small.webp'
 import project_4 from './assets/images/thumbnail-project-4-small.webp'
 import project_5 from './assets/images/thumbnail-project-5-small.webp'
 import project_6 from './assets/images/thumbnail-project-6-small.webp'
+import project_1_large from './assets/images/thumbnail-project-1-large.webp'
+import project_2_large from './assets/images/thumbnail-project-2-large.webp'
+import project_3_large from './assets/images/thumbnail-project-3-large.webp'
+import project_4_large from './assets/images/thumbnail-project-4-large.webp'
+import project_5_large from './assets/images/thumbnail-project-5-large.webp'
+import project_6_large from './assets/images/thumbnail-project-6-large.webp'
 
 export const projects = [
   {
     image: project_1,
-    title: 'Project 1',
+    title: 'Design Portfolio',
     skills: ['HTML', 'CSS', 'Javascript'],
   },
   {
     image: project_2,
-    title: 'Project 2',
+    title: 'E-Learning  landing page',
     skills: ['HTML', 'CSS', 'Javascript'],
   },
   {
     image: project_3,
-    title: 'Project 3',
+    title: 'todo web app',
     skills: ['HTML', 'CSS', 'Javascript'],
   },
   {
     image: project_4,
-    title: 'Project 4',
+    title: 'Entertainment web App',
     skills: ['HTML', 'CSS', 'Javascript'],
   },
   {
     image: project_5,
-    title: 'Project 5',
+    title: 'Memory Game',
     skills: ['HTML', 'CSS', 'Javascript'],
   },
   {
     image: project_6,
-    title: 'Project 6',
+    title: 'Art Gallery Showcase',
+    skills: ['HTML', 'CSS', 'Javascript'],
+  },
+]
+
+export const projects_large = [
+  {
+    image: project_1_large,
+    title: 'Design Portfolio',
+    skills: ['HTML', 'CSS', 'Javascript'],
+  },
+  {
+    image: project_2_large,
+    title: 'E-Learning  landing page',
+    skills: ['HTML', 'CSS', 'Javascript'],
+  },
+  {
+    image: project_3_large,
+    title: 'todo web app',
+    skills: ['HTML', 'CSS', 'Javascript'],
+  },
+  {
+    image: project_4_large,
+    title: 'Entertainment web App',
+    skills: ['HTML', 'CSS', 'Javascript'],
+  },
+  {
+    image: project_5_large,
+    title: 'Memory Game',
+    skills: ['HTML', 'CSS', 'Javascript'],
+  },
+  {
+    image: project_6_large,
+    title: 'Art Gallery Showcase',
     skills: ['HTML', 'CSS', 'Javascript'],
   },
 ]
